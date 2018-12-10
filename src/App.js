ef22b3fe0357954e458dbd1740f3859f43b8f3b6
Vadim <wadim.gaponow@mail.ru>
2018-12-10 20:59:11 +0300

    added first example of table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Example1 from './Tables/Example1';
+
 class App extends Component {
   render() {
     return (
@@ -19,6 +21,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Example1 />
         </header>
       </div>
     );
